af5f22f90a6f74f9819dba0953c9885e9533a0d2
Varga Benjamin <klim.bendzsi@gmail.com>
2020-02-22 19:43:13 +0100

    external event save to the database

---
+++ php/updateC.php
@@ -0,0 +1,24 @@
+<?php
+
+//update.php
+
+$connect = new PDO('mysql:host=localhost;dbname=ownfccustom', 'root', '');
+
+if(isset($_POST["id"]))
+{
+ $query = "
+ UPDATE events
+ SET title=:title, duration=:duration
+ WHERE id=:id
+ ";
+ $statement = $connect->prepare($query);
+ $statement->execute(
+  array(
+   ':title'  => $_POST['title'],
+   ':duration' => $_POST['duration'],
+   ':id'   => $_POST['id']
+  )
+ );
+}
+
+?>
