579f723cf0c89a5f9d877d2ac3cda91d25474544
Varga Benjamin <klim.bendzsi@gmail.com>
2020-04-16 15:55:38 +0200

    -comment out the pictures in the blog

--- parts/postLoads.php
+++ parts/postLoads.php
@@ -11,14 +11,14 @@ if ($stmt = $con->prepare('SELECT author, title, preview, maintext, publishtime 
 		$url = rawurlencode($row['title']);
 	  $output.=
 		'<div class="card mb-4">
-	   <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+	   <!--<img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">-->
 	   <div class="card-body">
 	     <h2 class="card-title">' . $row['title'] . '</h2>
 	     <p class="card-text">' . $row['preview'] . '</p>
-	     <a href="post.php?title=' . $url . '" class="btn btn-primary">Read More &rarr;</a>
+	     <a href="post.php?title=' . $url . '" class="btn btn-primary">Olvass róla többet &rarr;</a>
 	   </div>
 	   <div class="card-footer text-muted">
-	     Posted on ' . $row['publishtime'] . ' by
+	     Megosztva ' . $row['publishtime'] . ', szerző
 	     <a href="#">' . $row['author'] . '</a>
 	   </div>
 	  </div>';
