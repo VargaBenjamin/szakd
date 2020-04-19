4c7f440a576a72630728ef6a94f64d63c86899b3
Varga Benjamin <klim.bendzsi@gmail.com>
2020-04-19 22:33:17 +0200

    -home upgrade with newsfeed, and table

---
+++ parts/postLoadsMain.php
@@ -0,0 +1,33 @@
+<?php
+//postLoads.php
+require 'db.php';
+
+if ($stmt = $con->prepare('SELECT * FROM articles, accounts WHERE articles.authorid = accounts.id ORDER BY publishtime DESC LIMIT 1'))
+{
+  $stmt->execute();
+  $result = $stmt->get_result();
+	$output = '';
+  while ($row = $result->fetch_assoc()) {
+		$url = rawurlencode($row['title']);
+	  $output.=
+		'<div class="card mb-4">
+	   <img class="card-img-top" src="' . $row['picture'] . '" width="700" alt="Card image cap">
+	   <div class="card-body">
+	     <h2 class="card-title">' . $row['title'] . '</h2>
+	     <p class="card-text">' . $row['preview'] . '</p>
+	     <a href="post.php?title=' . $url . '" class="btn btn-primary">Olvass róla többet &rarr;</a>
+	   </div>
+	   <div class="card-footer text-muted">
+	     Megosztva ' . $row['publishtime'] . ', szerző
+	     <a href="#">' . $row['username'] . '</a>
+	   </div>
+	  </div>';
+  }
+	echo $output;
+}
+if ($stmt) {
+	$stmt->close();
+}
+$con->close();
+?>
+<!-- <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap"> -->
