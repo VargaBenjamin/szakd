af43a488ce5863397f9d804fac5ed424990a3a7d
Varga Benjamin <klim.bendzsi@gmail.com>
2020-04-19 12:55:36 +0200

    -change the empty checking with a php function

--- parts/settingsUpdate.php
+++ parts/settingsUpdate.php
@@ -3,42 +3,42 @@ require 'db.php';
 
 if(isset($_POST['id']))
 {
-  if ($_POST["username"] != "") {
+  if (!empty($_POST["username"])) {
     $username = mysqli_real_escape_string($con, $_POST["username"]);
     if ($stmt = $con->prepare('UPDATE accounts SET username = ? WHERE id ="' . $_POST["id"] . '"')) {
       $stmt->bind_param('s', $username);
       $stmt->execute();
     }
   }
-  if ($_POST["email"] != "") {
+  if (!empty($_POST["email"])) {
     $email = mysqli_real_escape_string($con, $_POST["email"]);
     if ($stmt = $con->prepare('UPDATE accounts SET email = ? WHERE id ="' . $_POST["id"] . '"')) {
       $stmt->bind_param('s', $email);
       $stmt->execute();
     }
   }
-  if ($_POST["telephone"] != "") {
+  if (!empty($_POST["telephone"])) {
     $telephone = mysqli_real_escape_string($con, $_POST["telephone"]);
     if ($stmt = $con->prepare('UPDATE accounts SET telephone = ? WHERE id ="' . $_POST["id"] . '"')) {
       $stmt->bind_param('s', $telephone);
       $stmt->execute();
     }
   }
-  if ($_POST["newPass"] != "") {
+  if (!empty($_POST["newPass"])) {
     $password = password_hash($_POST["newPass"], PASSWORD_DEFAULT);
     if ($stmt = $con->prepare('UPDATE accounts SET password = ? WHERE id ="' . $_POST["id"] . '"')) {
       $stmt->bind_param('s', $password);
       $stmt->execute();
     }
   }
-  if ($_POST["gym"] != "") {
+  if (!empty($_POST["gym"])) {
     $gym = mysqli_real_escape_string($con, $_POST["gym"]);
     if ($stmt = $con->prepare('UPDATE accounts SET gymid = ? WHERE id ="' . $_POST["id"] . '"')) {
       $stmt->bind_param('s', $gym);
       $stmt->execute();
     }
   }
-  if ($_POST["coach"] != "") {
+  if (!empty($_POST["coach"])) {
     $coach = mysqli_real_escape_string($con, $_POST["coach"]);
     if ($stmt = $con->prepare('UPDATE accounts SET coachid = ? WHERE id ="' . $_POST["id"] . '"')) {
       $stmt->bind_param('s', $coach);
