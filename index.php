e04b746e74b3a489abdfb07bc3571c080309eb22
Varga Benjamin <klim.bendzsi@gmail.com>
2020-04-19 20:31:52 +0200

    -change the index text

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Időpontfoglalás személyi edzéshez</title>
+  <title>Coatch2Client</title>
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -22,7 +22,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Coatch2Client</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -46,8 +46,8 @@
   </nav>
   <header class="bg-primary text-white">
     <div class="container text-center">
-      <h1>Welcome to Scrolling Nav</h1>
-      <p class="lead">A landing page template freshly redesigned for Bootstrap 4</p>
+      <h1>Üdvözöllek a Coatch2Client-en</h1>
+      <p class="lead">Egyedi webalkalmazás személyi edzők és vendégei részére</p>
     </div>
   </header>
 
@@ -56,12 +56,12 @@
       <div class="row">
         <div class="col-lg-8 mx-auto">
           <h2>Rólunk</h2>
-          <p class="lead">This is a great place to talk about your webpage. This template is purposefully unstyled so you can use it as a boilerplate or starting point for you own landing page designs! This template features:</p>
+          <p class="lead">Az alkalmazás segítségével lehetőség nyílik az edző és vendége közötti fal áttörésére:</p>
           <ul>
-            <li>Clickable nav links that smooth scroll to page sections</li>
-            <li>Responsive behavior when clicking nav links perfect for a one page website</li>
-            <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the navbar</li>
-            <li>Minimal custom CSS so you are free to explore your own unique design options</li>
+            <li>Saját felület kialakítása, ahol naplózni tudod eredményeidet, amiket a saját magad személyreszabott felütén tudsz többféle ábrákkal könnyen átlátni,</li>
+            <li>Közös naptár az edződdel, ahol egyszerűen tudsz időpontot foglalni hozzá, amit mind a ketten könnyedén átláthattok,</li>
+            <li>Modern, szép felületen,</li>
+            <li>Telefonon is könnyedén használható.</li>
           </ul>
         </div>
       </div>
@@ -73,7 +73,7 @@
       <div class="row">
         <div class="col-lg-8 mx-auto">
           <h2>Mit ajánlunk?</h2>
-          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
+          <p class="lead">Ingyenes regisztrálási lehetőséget mindkettőtök számára.</p>
         </div>
       </div>
     </div>
@@ -84,7 +84,7 @@
       <div class="row">
         <div class="col-lg-8 mx-auto">
           <h2>Lépj kapcsolatba velünk!</h2>
-          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
+          <p class="lead">Ha bármi kérdésed merül fel, írj nyugodtan, és kollégáink hamarosan jelentkeznek.</p>
         </div>
       </div>
     </div>
