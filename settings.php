1532df848b0550c6de32af6e37b6c34e9d41eb44
Varga Benjamin <klim.bendzsi@gmail.com>
2020-04-22 18:36:09 +0200

    -setting bootsrtap fix

--- settings.php
+++ settings.php
@@ -44,36 +44,36 @@ if ($stmt = $con->prepare('SELECT * FROM gym'))
                 </main>
 								<div class="container bootstrap snippets">
 								<div class="row">
-								  <div class="col-xs-12">
+								  <div class="col-sm-12">
 								    <form class="form" action="" method="post" id="settingsUpdate">
 											<div class="panel panel-default">
 								        <div class="panel-heading">
 								        <h4 class="panel-title">Fontosabb adatok</h4>
 								        </div>
 								        <div class="panel-body">
-								          <div class="form-group">
-								            <label class="col-md-12 control-label">Felhasználónév</label>
-														<div class="col-md-12">
+								          <div class="form-group row">
+								            <label class="col-md-4 control-label">Felhasználónév</label>
+														<div class="col-md-8">
 								              <input type="text" class="form-control" id="username">
 								            </div>
 								          </div>
-													<div class="form-group">
-								            <label class="col-md-12 control-label">Email cím</label>
-														<div class="col-md-12">
+													<div class="form-group row">
+								            <label class="col-md-4 control-label">Email cím</label>
+														<div class="col-md-8">
 								              <input type="email" class="form-control" id="email">
 								            </div>
 								          </div>
-													<div class="form-group">
-								            <label class="col-md-12 control-label">Edzőterem választás</label>
-														<div class="col-md-12">
+													<div class="form-group row">
+								            <label class="col-md-4 control-label">Edzőterem választás</label>
+														<div class="col-md-8">
 															<select class="form-control" id="gym">
 																<?php echo $gymoutput; ?>
 															</select>
 								            </div>
 								          </div>
-													<div id="coaches" class="form-group" style="display: none">
-								            <label class="col-md-12 control-label">Edző választás</label>
-														<div class="col-md-12">
+													<div class="form-group row" id="coaches" style="display: none">
+								            <label class="col-md-4 control-label">Edző választás</label>
+														<div class="col-md-8">
 															<select class="form-control" id="coach">
 															</select>
 								            </div>
@@ -86,20 +86,23 @@ if ($stmt = $con->prepare('SELECT * FROM gym'))
 								        <h4 class="panel-title">Egyéb adatok</h4>
 								        </div>
 								        <div class="panel-body">
-													<div class="form-group">
-								            <label class="col-md-12 control-label">Telefonszám</label>
-								            <div class="col-md-12">
+													<div class="form-group row">
+								            <label class="col-md-4 control-label">Telefonszám</label>
+								            <div class="col-md-8">
 								              <input type="tel" class="form-control" id="telephone">
 								            </div>
 								          </div>
-													<div class="form-group">
-														<button type="button" class="col-md-12 btn btn-danger" onclick="window.location.href='syncCallback.php?provider=Google'">Szinkronizálás a Google-lel</button>
+													<div class="form-group row">
+														<div class="col-md-4"></div>
+														<button type="button" class="col-md-8 btn btn-danger" onclick="window.location.href='syncCallback.php?provider=Google'">Szinkronizálás a Google-lel</button>
 													</div>
-													<div class="form-group">
-														<button type="button" class="col-md-12 btn btn-primary" onclick="window.location.href='syncCallback.php?provider=Facebook'">Szinkronizálás a Facebookkal</button>
+													<div class="form-group row">
+														<div class="col-md-4"></div>
+														<button type="button" class="col-md-8 btn btn-primary" onclick="window.location.href='syncCallback.php?provider=Facebook'">Szinkronizálás a Facebookkal</button>
 													</div>
-													<div class="form-group">
-														<button type="button" class="col-md-12 btn btn-info" onclick="window.location.href='syncCallback.php?provider=Twitter'">Szinkronizálás a Twitterrel</button>
+													<div class="form-group row">
+														<div class="col-md-4"></div>
+														<button type="button" class="col-md-8 btn btn-info" onclick="window.location.href='syncCallback.php?provider=Twitter'">Szinkronizálás a Twitterrel</button>
 													</div>
 								        </div>
 								      </div>
@@ -109,15 +112,15 @@ if ($stmt = $con->prepare('SELECT * FROM gym'))
 												<h4 class="panel-title">Jelszó</h4>
 												</div>
 												<div class="panel-body">
-													<div class="form-group">
-														<label class="col-md-12 control-label">Új jelszó</label>
-														<div class="col-md-12">
+													<div class="form-group row">
+														<label class="col-md-4 control-label">Új jelszó</label>
+														<div class="col-md-8">
 															<input type="password" class="form-control" id="newPass">
 														</div>
 													</div>
-													<div class="form-group">
-														<label class="col-md-12 control-label">Új jelszó újra</label>
-														<div class="col-md-12">
+													<div class="form-group row">
+														<label class="col-md-4 control-label">Új jelszó újra</label>
+														<div class="col-md-8">
 															<input type="password" class="form-control" id="newPassRe">
 														</div>
 													</div>
@@ -129,9 +132,9 @@ if ($stmt = $con->prepare('SELECT * FROM gym'))
 												<h4 class="panel-title">Mentéshez adja meg a jelenlegi jelszavát</h4>
 												</div>
 												<div class="panel-body">
-													<div class="form-group">
-														<label class="col-md-12 control-label">Jelszó</label>
-														<div class="col-md-12">
+													<div class="form-group row">
+														<label class="col-md-4 control-label">Jelszó</label>
+														<div class="col-md-8">
 															<input type="password" class="form-control" required id="pass">
 														</div>
 													</div>
