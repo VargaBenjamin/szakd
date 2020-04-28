838ab032bffe15702dc6f9b026fed6ce4167933b
Varga Benjamin <klim.bendzsi@gmail.com>
2020-04-28 10:46:21 +0200

    -remove the useless files

--- UselessFiles/tableUselessPhp/datatablesV.js
+++
@@ -1,9 +0,0 @@
-// Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable( {
-            responsive: true,
-            language: {
-               url: "https://cdn.datatables.net/plug-ins/1.10.20/i18n/Hungarian.json"
-            }
-        } );
-});
